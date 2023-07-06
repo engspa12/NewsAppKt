bb70c9886170994a61cdc84aee4e63fb8fda8a68
Daniel Bedoya <arturo.lpc12@gmail.com>
2023-07-06 18:22:53 -0500

    Update data package

--- app/src/main/java/com/example/newsappjetpackcompose/data/network/mapper/ArticleNetworkMapper.kt
+++ app/src/main/java/com/example/newsappjetpackcompose/data/network/mapper/ArticleNetworkMapper.kt
@@ -5,6 +5,14 @@ import com.example.newsappjetpackcompose.domain.model.ArticleDomain
 import com.example.newsappjetpackcompose.domain.util.NetworkMapper
 
 class ArticleNetworkMapper: NetworkMapper<ArticleNetwork, ArticleDomain> {
+
+    companion object {
+        const val TITLE_UNKNOWN = "Title Unknown"
+        const val SECTION_UNKNOWN = "Section Unknown"
+        const val AUTHOR_UNKNOWN = "Author Unknown"
+        const val DATE_UNKNOWN = "Date Unknown"
+        const val NO_IMAGE_AVAILABLE = "No image available"
+    }
     override fun mapToDomainModel(dto: ArticleNetwork): ArticleDomain {
         val sectionName = dto.sectionName
         val webUrl = dto.webUrl
@@ -12,16 +20,16 @@ class ArticleNetworkMapper: NetworkMapper<ArticleNetwork, ArticleDomain> {
 
         val fields = dto.fields
 
-        val author = if(fields?.byline.isNullOrEmpty()) "Unknown Author" else fields?.byline
-        val webTitle = if(fields?.headline.isNullOrEmpty()) "Unknown Title" else fields?.headline
-        val thumbnailUrl = if(fields?.thumbnail.isNullOrEmpty()) "No image available" else fields?.thumbnail
+        val author = if(fields?.byline.isNullOrEmpty()) AUTHOR_UNKNOWN else fields?.byline
+        val webTitle = if(fields?.headline.isNullOrEmpty()) TITLE_UNKNOWN else fields?.headline
+        val thumbnailUrl = if(fields?.thumbnail.isNullOrEmpty()) NO_IMAGE_AVAILABLE else fields?.thumbnail
 
         return ArticleDomain(
-            webTitle ?: "Title Unknown",
-            sectionName ?: "Section Unknown",
-            author ?: "Author Unknown",
-            publicationDate ?: "Date Unknown",
-            webUrl ?: "",
-            thumbnailUrl ?: "")
+            webTitle ?: TITLE_UNKNOWN,
+            sectionName ?: SECTION_UNKNOWN,
+            author ?: AUTHOR_UNKNOWN,
+            publicationDate ?: DATE_UNKNOWN,
+            webUrl.orEmpty(),
+            thumbnailUrl.orEmpty())
     }
 }
