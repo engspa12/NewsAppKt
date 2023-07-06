b963c973d1d437e18aaf96686450c2461de936c6
Daniel Bedoya <arturo.lpc12@gmail.com>
2023-07-06 18:22:10 -0500

    Update di package

--- app/src/main/java/com/example/newsappjetpackcompose/di/NewsHelpersModule.kt
+++ app/src/main/java/com/example/newsappjetpackcompose/di/NewsHelpersModule.kt
@@ -1,8 +1,9 @@
 package com.example.newsappjetpackcompose.di
 
 import android.content.Context
-import com.example.newsappjetpackcompose.util.ConnectionChecker
-import com.example.newsappjetpackcompose.util.ConnectionCheckerImpl
+import com.example.newsappjetpackcompose.domain.util.ConnectionChecker
+import com.example.newsappjetpackcompose.domain.util.ConnectionCheckerImpl
+import com.example.newsappjetpackcompose.domain.util.InputValidator
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,10 +12,15 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NewsCheckerModule {
+object NewsHelpersModule {
 
     @Provides
     fun provideConnectionChecker(@ApplicationContext appContext: Context): ConnectionChecker {
         return ConnectionCheckerImpl(appContext)
+    }
+
+    @Provides
+    fun provideInputValidator(): InputValidator {
+        return InputValidator()
     }
 }
--- app/src/main/java/com/example/newsappjetpackcompose/di/NewsServiceModule.kt
+++ app/src/main/java/com/example/newsappjetpackcompose/di/NewsServiceModule.kt
@@ -3,7 +3,8 @@ package com.example.newsappjetpackcompose.di
 import com.example.newsappjetpackcompose.domain.service.NewsService
 import com.example.newsappjetpackcompose.domain.service.NewsServiceImpl
 import com.example.newsappjetpackcompose.domain.interactor.NewsInteractor
-import com.example.newsappjetpackcompose.util.ConnectionChecker
+import com.example.newsappjetpackcompose.domain.util.ConnectionChecker
+import com.example.newsappjetpackcompose.domain.util.InputValidator
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +15,7 @@ import dagger.hilt.components.SingletonComponent
 object NewsServiceModule {
 
     @Provides
-    fun provideService(connectionChecker: ConnectionChecker, newsInteractor: NewsInteractor): NewsService {
-        return NewsServiceImpl(connectionChecker, newsInteractor)
+    fun provideService(connectionChecker: ConnectionChecker, inputValidator: InputValidator, newsInteractor: NewsInteractor): NewsService {
+        return NewsServiceImpl(connectionChecker, inputValidator, newsInteractor)
     }
 }
